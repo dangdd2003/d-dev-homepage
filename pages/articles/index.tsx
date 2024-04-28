f7d3d924382a77e04af596768c097dc551c351cd
Apollo-Dev <dangdoan2206@gmail.com>
2024-04-28 11:28:57 +0700

    fix import

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,6 +1,6 @@
 import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '@/components/layouts/article'
-import Section from '@/components/section'
+import { Section } from '@/components/section'
 import Paragraph from '@/components/paragraph'
 
 export default function Post() {
